1df07797e3c71c4a0bd84a3e77e7e2f04136464a
blaze <hardikkamaliya0000@gmail.com>
2025-05-21 13:43:01 +0530

    adding routing into pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Shop from "./pages/Shop"
+import Contact from "./pages/Contact"
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/shop" element={<Shop/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
